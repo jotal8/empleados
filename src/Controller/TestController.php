e45dd9a2e73772f9e213a777e9413f067d18af45
Julian Otalvaro <jotal8@gmail.com>
2024-12-13 11:58:24 -0500

    Se inicializa controlador para pruebas

---
+++ src/Controller/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/api/test", name="app_test")
+     */
+    public function app_test(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/TestController.php',
+        ]);
+    }
+}
