2c7933f7dded971061d9017e1c4131e521b7226a
Julian Otalvaro <jotal8@gmail.com>
2024-12-15 10:58:19 -0500

    Se refactoriza metodo para llamar el envio de email

---
+++ src/Services/CallEmailService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+
+class CallEmailService{
+
+    /**
+     * Llama el servicio que se encuentra con Python / Flask para envio de email
+     * con su respectiva autorizacion mediante token valido
+     * 
+     * @param string $name,
+     * @param string $password,
+     * @param string $token
+     */
+    public static function request($name, $password, $token): string
+    {
+        $Client = new Client();
+
+        try {
+            $headers = [
+                'Authorization' => $token
+            ];
+
+            $params = [
+                'name' => $name,
+                'password' => $password
+            ];
+
+            $clientRequest = $Client->request('POST', 'email-service:5000/sendEmail', [
+                'verify'      => false,
+                'headers'     => $headers,
+                'form_params' => $params
+            ]);
+
+            $response = $clientRequest->getBody();
+            $responseData = json_decode($response);
+        }catch(throwable $th){
+            return false;
+        }
+
+        return $responseData->message;
+    }
+}
